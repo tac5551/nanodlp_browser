3f754340629d886f24387f2d9b5d18f409a207ea
kunikata <kunikata@melody.international>
2022-02-28 19:09:20 +0900

    Add Icon Add Addform template

---
+++ trunk/UPnp WPF/AddMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace NanoDLP_Browser
+{
+    public partial class AddMachine : Form
+    {
+        public AddMachine()
+        {
+            InitializeComponent();
+        }
+        private Dto _dto;
+        public void setDto(Dto value) {
+            _dto = value;
+        }
+    }
+}
